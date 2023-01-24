27f0cd178d8a35dceea6cb1bc23458c4386216ae
Samson Orr <171129@student.vansd.org>
2023-01-24 20:30:30 +0000

    Any html works now.

--- script.js
+++ script.js
@@ -1,2 +1,2 @@
 var myWindow = window.open("", "");
-myWindow.document.write("<p>Content</p>");
+myWindow.document.write("<iframe width='100%' height='100%' src='content.html' frameborder='0' allowfullscreen></iframe>");
